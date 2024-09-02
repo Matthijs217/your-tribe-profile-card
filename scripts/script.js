e1d03a79fbfbdae1f6b7bc5bdd93506409a3e207
Matthijs217 <matthijstenbrink@gmail.com>
2024-09-02 15:45:03 +0200

    naam aangepast

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const expandButton = document.querySelector('header button') // de knop
+expandButton.addEventListener('click', expand) // als je klikt word het event expand aangeroepen
 
-function expand () {
+function expand () { //de functie om te expenden
   document.body.classList.toggle('expand')
 }
