a31956355d2ca88b0a1881ca3985d132f6d8dc31
Matthijs217 <matthijstenbrink@gmail.com>
2024-09-06 09:40:25 +0200

    visitekaartje2
    hjgyhvflyu

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,24 @@
-const expandButton = document.querySelector('header button') // de knop
-expandButton.addEventListener('click', expand) // als je klikt word het event expand aangeroepen
+var button = document.querySelector("button");
+var article = document.querySelector("article");
+var img = document.querySelector(".image-container");
+var h1 = document.querySelector("h1");
 
-function expand () { //de functie om te expenden
-  document.body.classList.toggle('expand')
-}
+
+var isChanged = false
+
+button.addEventListener("click", function() {
+
+    if (isChanged) {
+        article.style.background = "linear-gradient(to bottom, red, black)";
+        img.style.background = "linear-gradient(to bottom, red, black)";
+        h1.style.color = "black";
+    }
+    else {
+        article.style.background = "linear-gradient(0deg, rgb(28, 25, 123) 30%, #0bd2ff 100%)";
+        img.style.background = "linear-gradient(to bottom, rgb(13, 194, 243), rgb(33, 14, 133))";
+        h1.style.color = "rgba(0, 239, 255, 1)";
+    }
+
+    isChanged = !isChanged;
+
+  });
